96591acd3e006cebd17be7c9fca3ca3df022438b
arungopi <garunalways@disroot.org>
2023-03-14 08:09:40 +0530

    initial commit
    Python script for bulk fourier transform of SEM images.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# bulk-fast-fourier-transformer
- This python script crops all the FESEM images in a folder and then apply Fourier transform into each one of them.
+# Bulk-fast-fourier-transformer
+ This python script crops all the SEM images in a folder and then apply Fourier transform into each one of them. There are 
+many approches available in the market. Here, I tried to automate the process of cropping the databar of all the SEM images
+in the folder and then apply fast fourier transform to each image. Both cropped and fft images are saved in the same folder with proper suffix.
+
+# Basic usage 
+
+- Replace the path of the folder containing images in the script with your proper path. Then run the script. (Better to run script in main folder containing your image collectiion folder)
+- That's it. You are good to go.
+
+# Moderate usage
+
+- Set __plt.cmap__ values for different colour values.
+- To adjust crop dimension change values in __imcrop__ variable.
+- Adjust __plt.xlim__ and __plt.ylim__ for zoom in the given plot. (Most probably around center of the image)
+- Can remove __np.log__ to see non log scale. Means more black and white and less grey shades.
+
+## Sample output
+- Input image is in *.tif* format
+- Cropped image
+![Cropped](images/Sample_image_Cropped.png)
+
+- 2D FFT image
+![FFT](images/Sample_image_fourier.png)
+
+- Colourised
+![Colour FFT](images/Sample_image_fourier_colour.png)
+
+- Zoomed and coloured
+![Zoom](images/Sample_image_fourier_zoom.png)
+![Zoom colour](images/Sample_image_fourier_zoom_color.png)
+
+
+Made with ❤️ by [Arun Gopinathan](www.arungopinathan.com)
+
+
+
+
---
+++ main.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+#!interpreter [optional-arg]
+__author__ = "Arun Gopinathan"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Arun Gopinathan",'Rajeev Raveendran']
+__license__ = "GNU GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Arun Gopinath"
+__email__ = "garunalways@gmail.com"
+__status__ = "Production"
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+import os.path, sys
+
+
+# Add path containing FESEM images
+path = "images"
+dirs = os.listdir(path)
+
+# Function to calculate 2D Fourier Tranform
+def calculate_2dft(input):
+    ft = np.fft.ifftshift(input)
+    ft = np.fft.fft2(ft)
+    return np.fft.fftshift(ft)
+
+# This function will crop the image to remove FESEM databar and
+# each image is converted into Fourier Transform. Both images are savedin same folder
+def crop_ft():
+    for item in dirs:
+        fullpath = os.path.join(path,item)       
+        if os.path.isfile(fullpath):
+            im = Image.open(fullpath)
+            f, e = os.path.splitext(fullpath)
+            imCrop = im.crop((0, 0, 1024, 675)) # cropping databar
+            imCrop.save(f + '_Cropped.png', "png", quality=100) # saving cropped image as png
+
+            # Read and process image
+            image = mpimg.imread(fullpath)
+            image = image[:, :, :3].mean(axis=2)
+            plt.set_cmap("gray") # Try - gray, Greens, plasma, cividis, inferno, gist_earth (more on colormap - matplotlib)
+            ft = calculate_2dft(image)
+
+            # Plotting is done in log scale 
+            plt.imshow(np.log(abs(ft))) # You can remove np.log to check the normal results
+            #plt.axis("off") # To remove the axis
+
+            # If you are sure about the limits, then remove the hash and edit xlim and ylim (better around center)
+            #plt.xlim([400, 600])
+            #plt.ylim([300, 500])
+            plt.savefig(f + '_fourier.png') # Saving the final FT image
+crop_ft()
